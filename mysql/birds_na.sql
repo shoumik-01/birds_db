7ced45a7cc760314a8630c5687841f1e35642cba
Shoumik Hasan <50185501+shoumik-01@users.noreply.github.com>
2024-04-30 19:45:32 -0600

    Create birds_na.sql

---
+++ mysql/birds_na.sql
@@ -0,0 +1,52 @@
+CREATE DATABASE BIRDS_NA;
+USE BIRDS_NA;
+CREATE TABLE birds (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    common_name VARCHAR(100),
+    scientific_name VARCHAR(100),
+    weight VARCHAR(10),
+    length VARCHAR(10)
+);
+
+INSERT INTO birds (common_name, scientific_name, weight, length)
+VALUES
+('American Robin', 'Turdus migratorius', '77g', '25.5cm'),
+('Northern Cardinal', 'Cardinalis cardinalis', '44.8g', '23.5cm'),
+('Blue Jay', 'Cyanocitta cristata', '92.4g', '26cm'),
+('Steller''s Jay', 'Cyanocitta stelleri', '120g', '32cm'),
+('Mourning Dove', 'Zenaida macroura', '128g', '31cm'),
+('American Crow', 'Corvus brachyrhynchos', '468g', '46.5cm'),
+('European Starling', 'Sturnus vulgaris', '79.5g', '21cm'),
+('Northern Mockingbird', 'Mimus polyglottos', '49g', '24.3cm'),
+('Black-billed Magpie', 'Pica hudsonia', '175.8g', '52.5cm'),
+('Dark-eyed Junco', 'Junco hyemalis', '24g', '15.3cm'),
+('Black-capped Chickadee', 'Poecile atricapillus', '12g', '13.5cm'),
+('White-breasted Nuthatch', 'Sitta carolinensis', '24g', '15.5cm'),
+('Tufted Titmouse', 'Baeolophus bicolor', '21.5g', '15cm'),
+('House Sparrow', 'Passer domesticus', '31.8g', '16cm'),
+('House Wren', 'Troglodytes aedon', '11g', '12cm'),
+('House Finch', 'Haemorhous mexicanus', '21g', '13.8cm'),
+('American Goldfinch', 'Spinus tristis', '15.5g', '12.5cm'),
+('Downy Woodpecker', 'Dryobates pubescens', '26.5g', '16cm'),
+('Hairy Woodpecker', 'Leuconotopicus villosus', '67.5g', '22cm'),
+('Red-bellied Woodpecker', 'Melanerpes carolinus', '74g', '24.8cm'),
+('American kestrel', 'Falco sparverius', '120g', '26.5cm'),
+('Ruby-throated hummingbird', 'Archilochus colubris', '3.6g', '8cm'),
+('Carolina wren', 'Thryothorus ludovicianus', '20.5g', '26.5cm'),
+('Spotted Towhee', 'Pipilo maculatus', '41g', '19cm'),
+('Lesser Goldfinch', 'Spinus psaltria', '9.8g', '10.5cm'),
+('Barn swallow', 'Hirundo rustica', '19g', '18cm'),
+('Cliff swallow', 'Petrochelidon pyrrhonota', '25g', '13cm'),
+('Tree swallow', 'Tachycineta bicolor', '21.3g', '13cm'),
+('Pine siskin', 'Spinus pinus', '15g', '12.5cm'),
+('Purple finch', 'Haemorhous purpureus', '23.3g', '14cm'),
+('Red-winged blackbird', 'Agelaius phoeniceus', '64g', '23cm'),
+('Eastern bluebird', 'Sialia sialis', '30.5g', '18.5cm'),
+('Gray catbird', 'Dumetella carolinensis', '37.5g', '22.3cm'),
+('Brown creeper', 'Certhia americana', '7.1g', '13cm'),
+('Common yellowthroat', 'Geothlypis trichas', '8.5g', '12cm'),
+('American redstart', 'Setophaga ruticilla', '8.6g', '12.5cm'),
+('American woodcock', 'Scolopax minor', '185g', '27.5cm'),
+('Baltimore oriole', 'Icterus galbula', '33.8g', '19.5cm'),
+('Cedar waxwing', 'Bombycilla cedrorum', '30g', '16.5cm'),
+('Indigo bunting', 'Passerina cyanea', '14.5g', '13.3cm');
